0946406f6d3ce873a15e09a794eb36b81b85093b
jkamiya5 <jkamiya5@gmail.com>
2014-09-25 10:08:22 +0900

    こみっと

---
+++ SojiToban/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SojiToban.ViewModels
+{
+    //public class MainWindowViewModel
+    //{
+    //    public MainWindowViewModel()
+    //    {
+    //        if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+    //        {
+    //            Text = "あいうえお";
+    //        }
+    //    }
+
+    //    public string Text { get; set; }
+
+    //    private ViewModelCommand printCommand;
+
+    //    public ViewModelCommand PrintCommand
+    //    {
+    //        get { return this.printCommand ?? (this.printCommand = new ViewModelCommand(this.Print)); }
+    //    }
+
+    //    private void Print()
+    //    {
+    //        Printer.Print(this);
+    //    }
+    //}
+}
