f3bb68de5e61396d97a26bf3d02625195336f1f7
Joji Kamiya <jkamiya5@gmail.com>
2014-10-19 20:53:15 +0900

    こみっと

---
+++ SojiTobanTest/SameDayAssignmentJudgeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SojiToban.CommonModule;
+using SojiToban.Dto;
+
+namespace SojiTobanTest
+{
+    [TestClass]
+    public class SameDayAssignmentJudgeTest
+    {
+        [TestMethod]
+        public void CheckSameDayAssignmentJudge()
+        {
+            Member member = new Member();            
+            ContractConst.DAYS? targetDay = ContractConst.DAYS.月;
+            Assert.IsTrue(SameDayAssignmentJudge.Judge(member, targetDay));
+        }
+    }
+}
