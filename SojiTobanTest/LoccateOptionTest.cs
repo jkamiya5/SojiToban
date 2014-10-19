f18650f477e63756c97ec30b5c00642e4fbc9ca7
Joji Kamiya <jkamiya5@gmail.com>
2014-10-19 20:52:52 +0900

    こみっと

---
+++ SojiTobanTest/LoccateOptionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SojiToban.CommonModule;
+using SojiToban.Dto;
+using System.Collections.Generic;
+using SojiToban.Service;
+
+namespace SojiTobanTest
+{
+    [TestClass]
+    public class LoccateOptionTest
+    {
+        [TestMethod]
+        public void CheckCheckCleanable1()
+        {
+
+            LoccateOption target = new LoccateOption();
+            Member member = new Member();
+            int? randamPlaceValue = 0;
+            ContractConst.DAYS dAYS = ContractConst.DAYS.月;
+            Assert.IsFalse(target.CheckCleanable(member, randamPlaceValue, dAYS));
+
+
+        }
+    }
+}
