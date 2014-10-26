0b0b54a4e3e1d9b1ab8ad47c5c2a6fc68fc58d54
Joji Kamiya <jkamiya5@gmail.com>
2014-10-26 21:28:08 +0900

    こみっと

---
+++ SojiTobanTest/DataOptionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SojiToban.CommonModule;
+using SojiToban.Dto;
+
+namespace SojiTobanTest
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class DataOptionTest
+    {
+
+        [TestMethod]
+        public void getTeamDataMethodTest()
+        {
+
+        }
+    }
+}
